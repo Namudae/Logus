397f93b0d7d63e29c8a5e3dbe0b187c3a121d021
frotage0112@gmail.com <frotage0112@gmail.com>
2024-08-14 17:50:42 +0900

    [new] querydsl 의존성 추가

--- src/main/java/com/logus/blog/repository/MemberRepository.java
+++ src/main/java/com/logus/blog/repository/MemberRepository.java
@@ -5,32 +5,33 @@ import com.logus.blog.domain.Member;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
-@RequiredArgsConstructor
-public class MemberRepository {
+//@Repository
+//@RequiredArgsConstructor
+public interface MemberRepository extends JpaRepository<Member, Long> {
 
-    private final EntityManager em;
-
-    public void save(Member member) {
-        em.persist(member);
-    }
-
-    public Member findOne(Long id) {
-        return em.find(Member.class, id); //Member 찾아서 반환
-    }
-
-    public List<Member> findAll() {
-        return em.createQuery("select m from Member m", Member.class) // (jpql, 반환타입)
-                .getResultList();
-    }
-
-    public List<Member> findByNickname(String nickname) {
-        return em.createQuery("select m from Member m where m.nickname = :nickname", Member.class)
-                .setParameter("nickname", nickname)
-                .getResultList();
-    }
+//    private final EntityManager em;
+//
+//    public void save(Member member) {
+//        em.persist(member);
+//    }
+//
+//    public Member findOne(Long id) {
+//        return em.find(Member.class, id); //Member 찾아서 반환
+//    }
+//
+//    public List<Member> findAll() {
+//        return em.createQuery("select m from Member m", Member.class) // (jpql, 반환타입)
+//                .getResultList();
+//    }
+//
+//    public List<Member> findByNickname(String nickname) {
+//        return em.createQuery("select m from Member m where m.nickname = :nickname", Member.class)
+//                .setParameter("nickname", nickname)
+//                .getResultList();
+//    }
 }
