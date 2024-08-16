abaf7333142c29e9fc969a5f62e96572e9efe209
frotage0112@gmail.com <frotage0112@gmail.com>
2024-08-16 17:55:15 +0900

    [refactor] post_tag 엔티티 수정

--- src/main/java/com/logus/blog/service/MemberService.java
+++ src/main/java/com/logus/blog/service/MemberService.java
@@ -10,39 +10,39 @@ import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
-@RequiredArgsConstructor //final 있는 필드로 생성자 만들어줌
+@RequiredArgsConstructor
 public class MemberService {
 
-    private final MemberRepository memberRepository; //final 어노테이션 넣는 것이 좋음
+    private final MemberRepository memberRepository;
 
 
     /**
      * 회원 가입
      */
-    @Transactional
-    public Long join(Member member) {
-
-        validateDuplicateMember(member); //중복 회원 검증
-        memberRepository.save(member);
-        return member.getId();
-    }
-
-    private void validateDuplicateMember(Member member) {
-        //EXCEPTION
-        List<Member> findMembers = memberRepository.findByNickname(member.getNickname());
-        if (!findMembers.isEmpty()) {
-            throw new IllegalStateException("이미 존재하는 회원입니다.");
-        }
-    }
-
-    //회원 전체 조회
-    public List<Member> findMembers() {
-        return memberRepository.findAll();
-    }
-
-    //회원 단건 조회
-    public Member findOne(Long memberId) {
-        return memberRepository.findOne(memberId);
-    }
+//    @Transactional
+//    public Long join(Member member) {
+//
+//        validateDuplicateMember(member); //중복 회원 검증
+//        memberRepository.save(member);
+//        return member.getId();
+//    }
+//
+//    private void validateDuplicateMember(Member member) {
+//        //EXCEPTION
+//        List<Member> findMembers = memberRepository.findByNickname(member.getNickname());
+//        if (!findMembers.isEmpty()) {
+//            throw new IllegalStateException("이미 존재하는 회원입니다.");
+//        }
+//    }
+//
+//    //회원 전체 조회
+//    public List<Member> findMembers() {
+//        return memberRepository.findAll();
+//    }
+//
+//    //회원 단건 조회
+//    public Member findOne(Long memberId) {
+//        return memberRepository.findOne(memberId);
+//    }
 
 }
