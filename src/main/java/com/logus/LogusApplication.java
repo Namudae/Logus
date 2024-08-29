ec8a02bb35081fcb2920fb19a17d1c4d8ff4113d
frotage0112@gmail.com <frotage0112@gmail.com>
2024-08-29 17:32:38 +0900

    [Rename] LogusApplication 경로이동

--- src/main/java/com/logus/LogusApplication.java
+++ src/main/java/com/logus/LogusApplication.java
@@ -1,11 +1,11 @@
-package com.logus.blog;
+package com.logus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
 public class LogusApplication {
-
 
 	public static void main(String[] args) {
 		SpringApplication.run(LogusApplication.class, args);
