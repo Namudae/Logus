8a5779b3fe360c6c663e982139446a3c94d5c936
frotage0112@gmail.com <frotage0112@gmail.com>
2024-10-02 11:26:27 +0900

    refeat: 에러코드 수정

--- src/main/java/com/logus/common/exception/ErrorCode.java
+++ src/main/java/com/logus/common/exception/ErrorCode.java
@@ -11,16 +11,16 @@ public enum ErrorCode {
     /* 1001~: 공통 */
     INPUT_VALUE_INVALID(1001, HttpStatus.BAD_REQUEST, "입력값이 올바르지 않습니다."),
 
-    /* 500 GLOBAL */
-    INTERNAL_SERVER_ERROR(500, HttpStatus.INTERNAL_SERVER_ERROR, "내부 서버 오류입니다."),
+    /* 2001~: 회원 */
+    MEMBER_NOT_FOUND(404, HttpStatus.NOT_FOUND, "존재하지 않는 회원입니다."),
 
 
-    /* 404 NOT_FOUND: Resource를 찾을 수 없음 */
-    BLOG_NOT_FOUND(404, HttpStatus.NOT_FOUND, "존재하지 않는 페이지입니다."),
-    POST_NOT_FOUND(404, HttpStatus.NOT_FOUND, "존재하지 않는 페이지입니다."),
-    MEMBER_NOT_FOUND(404, HttpStatus.NOT_FOUND, "존재하지 않는 회원입니다.");
+    /* 3001~: 블로그 */
+    BLOG_NOT_FOUND(3001, HttpStatus.NOT_FOUND, "존재하지 않는 페이지입니다."),
+    POST_NOT_FOUND(3002, HttpStatus.NOT_FOUND, "존재하지 않는 페이지입니다."),
 
-    /* 409 CONFLICT : Resource 의 현재 상태와 충돌. 보통 중복된 데이터 존재 */
+
+    INTERNAL_SERVER_ERROR(500, HttpStatus.INTERNAL_SERVER_ERROR, "내부 서버 오류입니다.");
 
     private final int code;
     private final HttpStatus status;
