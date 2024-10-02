77f3de7c4c5583ceb382f3fa32a042eacb4d0b6f
frotage0112@gmail.com <frotage0112@gmail.com>
2024-10-02 11:31:54 +0900

    refeat: 에러코드 수정

--- src/main/java/com/logus/common/exception/ErrorCode.java
+++ src/main/java/com/logus/common/exception/ErrorCode.java
@@ -12,14 +12,13 @@ public enum ErrorCode {
     INPUT_VALUE_INVALID(1001, HttpStatus.BAD_REQUEST, "입력값이 올바르지 않습니다."),
 
     /* 2001~: 회원 */
-    MEMBER_NOT_FOUND(404, HttpStatus.NOT_FOUND, "존재하지 않는 회원입니다."),
-
+    MEMBER_NOT_FOUND(2001, HttpStatus.NOT_FOUND, "존재하지 않는 회원입니다."),
 
     /* 3001~: 블로그 */
     BLOG_NOT_FOUND(3001, HttpStatus.NOT_FOUND, "존재하지 않는 페이지입니다."),
     POST_NOT_FOUND(3002, HttpStatus.NOT_FOUND, "존재하지 않는 페이지입니다."),
 
-
+    /* 1000번대 이하: 상태 코드 */
     INTERNAL_SERVER_ERROR(500, HttpStatus.INTERNAL_SERVER_ERROR, "내부 서버 오류입니다.");
 
     private final int code;
