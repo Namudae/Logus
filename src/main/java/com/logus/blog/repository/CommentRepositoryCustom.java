13e2e234505c9cbad86e47e8013cf05e0ebc37f5
frotage0112@gmail.com <frotage0112@gmail.com>
2024-10-16 16:04:28 +0900

    feat: 비밀댓글 처리

---
+++ src/main/java/com/logus/blog/repository/CommentRepositoryCustom.java
@@ -0,0 +1,10 @@
+package com.logus.blog.repository;
+
+import com.logus.blog.dto.CommentResponseDto;
+import com.logus.blog.entity.Comment;
+
+import java.util.List;
+
+public interface CommentRepositoryCustom {
+//    List<CommentResponseDto> selectComments(Long postId);
+}
