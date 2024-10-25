79aa93947cb3afb8ef2b854a9c684c3b95bb9a99
frotage0112@gmail.com <frotage0112@gmail.com>
2024-10-25 11:27:41 +0900

    refactor: 블로그 등록/수정 정규식 수정

--- src/main/java/com/logus/blog/dto/BlogRequestDto.java
+++ src/main/java/com/logus/blog/dto/BlogRequestDto.java
@@ -18,11 +18,12 @@ public class BlogRequestDto {
 
     @NotBlank(message = "블로그명을 입력하세요.")
     @Size(max=20, message = "블로그명을 20자 이내로 작성해 주세요.")
+    @Pattern(regexp = "^[가-힣a-zA-Z0-9\\s-_]+$", message = "한글, 영문, 숫자, 특수문자(-, _)를 사용하여 입력해 주세요.")
     private String blogName;
 
     @NotBlank(message = "블로그 주소를 입력하세요.")
-    @Size(max=30, message = "블로그 주소를 30자 이내로 작성해 주세요.")
-    @Pattern(regexp = "^[a-zA-Z0-9_]+$", message = "영어, 숫자, 언더바(_)만 사용 가능합니다.")
+    @Size(min=4, max=30, message = "블로그 주소 4-32자를 입력해 주세요.")
+    @Pattern(regexp = "^[a-z0-9-]+$", message = "영문 소문자, 숫자, 특수문자(-)를 사용하여 입력해 주세요.")
     private String blogAddress;
 
     @Size(max=100, message = "블로그 소개를 100자 이내로 작성해 주세요.")
