159a847230af885af0a063a04bf3ef3cf3df4f91
frotage0112@gmail.com <frotage0112@gmail.com>
2024-11-27 16:00:42 +0900

    fix: 시리즈 수정

--- src/main/java/com/logus/blog/entity/Series.java
+++ src/main/java/com/logus/blog/entity/Series.java
@@ -38,9 +38,9 @@ public class Series {
     }
 
     public void updateSeries(SeriesRequestDto seriesRequestDto, String imgUrl) {
-        this.seriesName = seriesRequestDto.getSeriesName();
-        this.seriesOrder = seriesRequestDto.getSeriesOrder();
-        this.imgUrl = imgUrl;
+        this.seriesName = seriesRequestDto.getSeriesName() != null ? seriesRequestDto.getSeriesName() : this.getSeriesName();
+        this.seriesOrder = seriesRequestDto.getSeriesOrder() != null ? seriesRequestDto.getSeriesOrder() : this.seriesOrder;
+        this.imgUrl = imgUrl != null ? imgUrl : this.imgUrl;
     }
 
     public void updateSeries(SeriesOrderRequestDto.SeriesDto seriesRequestDto) {
