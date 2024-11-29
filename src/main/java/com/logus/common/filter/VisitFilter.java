cea64e6b18d36eaf178508706e59ce3266f6c494
frotage0112@gmail.com <frotage0112@gmail.com>
2024-11-29 13:50:00 +0900

    refactor: visit 주석

--- src/main/java/com/logus/common/filter/VisitFilter.java
+++ src/main/java/com/logus/common/filter/VisitFilter.java
@@ -11,7 +11,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
-@Component
+//@Component
 public class VisitFilter extends OncePerRequestFilter {
 
     @Autowired
